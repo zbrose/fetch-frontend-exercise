4d4a56581e5888be82dc806d7b10cec6743d9709
Zach Brose <97310057+zbrose@users.noreply.github.com>
2025-02-06 11:48:45 -0800

    adds user authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          <h1>Alley Cat's Sibling Adoption Agency</h1>
+        </header>
         {children}
       </body>
     </html>
